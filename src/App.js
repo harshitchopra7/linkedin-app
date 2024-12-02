93f3879ccabf3c94b427ba1df74a7d8e7b5314dc
harshitchopra7 <chopraharshit2000@gmail.com>
2024-12-02 15:45:59 +0530

    create navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* Navbar  */}
+      <Navbar />
+
+      {/* Homepage = 
+      Left Pane -
+        Profile Summary 
+        Profile Statistics
+        Linkedin Premium Ad
+        Bottom Left Pane
+      Main Feed -
+        Create Post Panel
+        Sorting of Posts 
+        Posts Feed
+      Right Pane -
+        Trending Now Section
+        Promoted Page Ad
+        Footer */}
     </div>
   );
 }
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,106 @@
+import React from "react";
+
+// styles
+import "./Navbar.css";
+
+// mui icons
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import HomeIcon from "@mui/icons-material/Home";
+import PeopleAltIcon from "@mui/icons-material/PeopleAlt";
+import WorkIcon from "@mui/icons-material/Work";
+import TextsmsIcon from "@mui/icons-material/Textsms";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import SearchIcon from "@mui/icons-material/Search";
+import AppsIcon from "@mui/icons-material/Apps";
+import SquareIcon from "@mui/icons-material/Square";
+
+function Navbar() {
+  const ICON_DIMENSIONS = "28px";
+
+  const styles = {
+    height: ICON_DIMENSIONS,
+    width: ICON_DIMENSIONS,
+    color: "gray",
+  };
+
+  const navItems = [
+    {
+      id: 1,
+      icon: <HomeIcon style={styles} />,
+      text: "Home",
+    },
+    {
+      id: 2,
+      icon: <PeopleAltIcon style={styles} />,
+      text: "My Network",
+    },
+    {
+      id: 3,
+      icon: <WorkIcon style={styles} />,
+      text: "Jobs",
+    },
+    {
+      id: 4,
+      icon: <TextsmsIcon style={styles} />,
+      text: "Messaging",
+    },
+    {
+      id: 5,
+      icon: <NotificationsIcon style={styles} />,
+      text: "Notifications",
+    },
+    {
+      id: 6,
+      icon: <AccountCircleIcon style={styles} />,
+      text: "Me",
+    },
+  ];
+
+  return (
+    <div className="navContainer">
+      {/* Left Side  */}
+      <div className="navLeft">
+        <LinkedInIcon
+          style={{ color: "#0A66C2", height: "45px", width: "45px" }}
+        />
+        <div className="navLeftInputContainer">
+          <SearchIcon
+            style={{ position: "absolute", top: "5px", padding: "0 10px" }}
+          />
+          <input className="navLeftInput" placeholder="Search" />
+        </div>
+      </div>
+
+      {/* Right Side  */}
+      <div className="navRight">
+        <div className="navItems">
+          {navItems.map((navItem, key) => (
+            <div className="navItemContainer">
+              <div className="navItemsIcon">{navItem.icon}</div>
+              <p className="navItemsText">{navItem.text}</p>
+            </div>
+          ))}
+        </div>
+
+        <div className="navRightBusinessContainer">
+          <div className="navItemContainer">
+            <div className="navItemsIcon">
+              <AppsIcon style={styles} />
+            </div>
+            <p className="navItemsText">For Business</p>
+          </div>
+
+          <div className="navItemContainer tryPremiumIcon">
+            <div className="navItemsIcon">
+              <SquareIcon style={styles} />
+            </div>
+            <p className="navItemsText">Try Premium for ₹0</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
